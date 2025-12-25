0ae26c1fbdbe5a25a9df24855ec282db3e738f98
Ayo <Ayoroq@gmail.com>
2025-12-24 20:12:11 -0500

    feat: implement signup form with validation and data handling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,43 @@
 export default function Signup() {
-    return <h1>Signup Page</h1>
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+    }
+    return (
+        <main>
+            <h1>Signup Page</h1>
+            <p>This is the signup page</p>
+            <form action="" onSubmit={handleFormSubmit}>
+                <p>
+                    <label htmlFor="firstName"></label>
+                    <input type="text" name="firstName" id="firstName" placeholder="First Name" required />
+                </p>
+                <p>
+                    <label htmlFor="lastName"></label>
+                    <input type="text" name="lastName" id="lastName" placeholder="Last Name" required />
+                </p>
+                <p>
+                    <label htmlFor="email"></label>
+                    <input type="email" name="email" id="email" placeholder="Email" required />
+                </p>
+                <p>
+                    <label htmlFor="username"></label>
+                    <input type="text" name="username" id="username" placeholder="Username" required />
+                </p>
+                <p>
+                    <label htmlFor="password"></label>
+                    <input type="password" name="password" id="password" placeholder="Password" required />
+                </p>
+                <p>
+                    <label htmlFor="confirmPassword"></label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" required />
+                </p>
+                <p>
+                    <button type="submit">Signup</button>
+                </p>
+            </form>
+        </main>
+    )
 }
