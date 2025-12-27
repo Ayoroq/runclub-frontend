54f663ef81e53ccfad4d91250ce537bf50ab28f6
Ayo <Ayoroq@gmail.com>
2025-12-27 14:55:40 -0500

    Added the api call to show the posts and also the call fixed syntax issue in navbar svg

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -2,18 +2,40 @@ import styles from "./pages.module.css";
 import Button from "../components/Button";
 import buttonStyles from "../components/Button.module.css";
 import { useNavigate } from "react-router";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export default function Dashboard() {
   const navigate = useNavigate();
   const { isLoggedIn, loading } = useContext(AuthContext);
+    const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     if (!loading && !isLoggedIn) {
       navigate("/login");
     }
   }, [isLoggedIn, loading, navigate]);
+
+useEffect(() => {
+    async function getPosts() {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+          method: 'GET',
+          credentials: 'include'
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+        } else {
+          console.error('Server returned an error:', res.status);
+        }
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+    getPosts();
+  }, []);
 
   if(loading) return (null)
   
