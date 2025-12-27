0c45bdb2d6112807384479ca8258786e8eea7ab0
Ayo <Ayoroq@gmail.com>
2025-12-27 17:21:33 -0500

    Added lenis to the app for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useEffect,useRef } from "react";
 import { Outlet, useLocation } from "react-router";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
+import { gsap } from "gsap";
+import { ReactLenis } from "lenis/react";
 
 function App() {
+  const lenisRef = useRef();
   const location = useLocation();
   const hideNavbar = ["/login", "/signup"].includes(location.pathname);
 
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
     <AuthProvider>
       <>
         {!hideNavbar && <Navbar />}
         <Outlet />
       </>
     </AuthProvider>
+    </ReactLenis>
   );
 }
 
