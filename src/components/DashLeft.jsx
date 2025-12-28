4534cbd1fb9d4e164df1c2ea98cd1e087e7c1131
Ayo <Ayoroq@gmail.com>
2025-12-28 15:32:41 -0500

    Added more stylings to the post button

--- src/components/DashLeft.jsx
+++ src/components/DashLeft.jsx
@@ -2,31 +2,36 @@ import styles from './Components.module.css'
 export default function DashLeft(){
     return(
         <section className={styles.leftNavContainer}>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.active} ${styles.leftNav}`}>Home</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Explore</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Notifications</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Messages</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Profile</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Communities</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Bookmarks</p>
             </div>
-            <div>
+            <div className={styles.leftNavItem}>
                 <p className={`${styles.leftNav}`}>Settings</p>
             </div>
-            <button className={styles.postButton}>Post</button>
+            <div className={styles.leftNavItem}>
+                <p className={`${styles.leftNav}`}>Help</p>
+            </div>
+            <div className={styles.leftNavItem}>
+                <button className={styles.postButton}>Post</button>
+            </div>
         </section>
     )
 }
