f1db3de88d748a6804ec2559cc9c6f071cebcda4
Ayo <Ayoroq@gmail.com>
2025-12-29 03:36:24 -0500

    Added image to the write and also changed signup button colour

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import styles from "./pages.module.css";
 import Button from "../components/Button.jsx";
 import buttonStyles from "../components/Button.module.css";
 import { useNavigate } from "react-router";
-import {use, useContext, useEffect} from 'react'
+import { use, useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext.jsx";
 
 export default function Signup() {
@@ -25,65 +25,69 @@ export default function Signup() {
       console.error("Signup failed");
     }
   }
-  
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <main className={styles.main}>
-      <h1>Signup Page</h1>
-      <p>This is the signup page</p>
-      <form action="" onSubmit={handleFormSubmit} className={styles.form}>
-        <p>
-          <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" id="firstName" required />
-        </p>
-        <p>
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" id="lastName" required />
-        </p>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" required />
-        </p>
-        <p>
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" required />
-        </p>
-        <p>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            required
-            minLength={8}
+    <main className={styles.signupMain}>
+      <div className={styles.signupContainer}>
+        <h1>Ready to Start your Running Journey?</h1>
+        <p className={styles.signupText}>Sign up to our run club now to join our community!</p>
+        <form action="" onSubmit={handleFormSubmit} className={styles.form}>
+          <p>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" name="firstName" id="firstName" required />
+          </p>
+          <p>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" name="lastName" id="lastName" required />
+          </p>
+          <p>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required />
+          </p>
+          <p>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" required />
+          </p>
+          <p>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              minLength={8}
+            />
+          </p>
+          <p>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              required
+              minLength={8}
+            />
+          </p>
+          <Button
+            type="submit"
+            className={buttonStyles.signupButton}
+            text="Signup"
           />
-        </p>
+        </form>
         <p>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            required
-            minLength={8}
-          />
+          Already have an account?{" "}
+          <span className={styles.loginLink} onClick={() => navigate("/login")}>
+            Login
+          </span>
         </p>
-        <Button
-          type="submit"
-          className={buttonStyles.signupButton}
-          text="Signup"
-          color="green"
-        />
-      </form>
-      <p>
-        Already have an account?{" "}
-        <span className={styles.loginLink} onClick={() => navigate("/login")}>
-          Login
-        </span>
-      </p>
+      </div>
+      <div className={styles.signUpImageContainer}>
+        <img className={styles.signUpImage} src="/signup-run.jpg" alt="A man running" />
+      </div>
     </main>
   );
 }
