511be67b0e9e758dc00863dd677edcb45fe40cba
Ayo <Ayoroq@gmail.com>
2025-12-30 14:52:53 -0500

    Added the option to close the dropdown with external clicks

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,8 +1,9 @@
 import styles from "./Components.module.css";
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function MobileNav() {
-  const buttonRef = useRef(null)
+  const buttonRef = useRef(null);
+  const menuRef = useRef(null);
   const [isDropDownActive, setIsDropDownActive] = useState(false);
   function handleHomeClick() {
     window.scrollTo({
@@ -11,6 +12,23 @@ export default function MobileNav() {
       behavior: "smooth",
     });
   }
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        isDropDownActive &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setIsDropDownActive(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isDropDownActive]);
+
   return (
     <main className={styles.bottomNav}>
       <svg
@@ -56,13 +74,16 @@ export default function MobileNav() {
           aria-hidden="true"
           className={`${styles.bottomNavIcon} ${styles.mobileMoreButton}`}
           onClick={() => setIsDropDownActive(!isDropDownActive)}
+          ref={menuRef}
         >
           <g>
             <path d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"></path>
           </g>
         </svg>
-        <div style={isDropDownActive ? { display: "flex" } : undefined}
-                  className={styles.DropDownContainer}>
+        <div
+          style={isDropDownActive ? { display: "flex" } : undefined}
+          className={styles.DropDownContainer}
+        >
           <div>
             <p>Want to be become a member?</p>
             <button ref={buttonRef} className={styles.subscribeButton}>
