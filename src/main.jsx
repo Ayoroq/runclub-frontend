fb23e59fd79a143f68ee4d1c20f101ea078e88c1
Ayo <Ayoroq@gmail.com>
2025-12-31 22:03:19 -0500

    Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import routers from '../src/routes/routes.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 const router = createBrowserRouter(routers)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>,
 )
